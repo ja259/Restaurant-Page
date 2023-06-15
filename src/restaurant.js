bbc23c1f8ca7a714b60c20840d74fe987db9c95e
James Otwane <127046977+ja259@users.noreply.github.com>
2023-06-15 19:48:23 -0400

    Add package.json

---
+++ src/restaurant.js
@@ -0,0 +1,27 @@
+// restaurant.js
+
+const createRestaurantHomePage = () => {
+    const content = document.querySelector("#content");
+    const pageContent = document.createElement('div');
+    pageContent.classList.add('page-content');
+
+    //Create and append image elemtent
+    const image = document.createElement('img');
+    image.src = 'images/food.jpg';
+    image.height = '300';
+    image.width = '300';
+    pageContent.appendChild(image);
+
+    //Create and append headline element
+    const headline = document.createElement('h1');
+    headline.textContent = 'Welcome to our restaurant!';
+    pageContent.appendChild(headline);
+
+    //Create and append copy element
+    const copy = document.createElement('p');
+    copy.textContent = 'We serve the best food in town. Come and taste it!';
+    pageContent.appendChild(copy);
+    content.appendChild(pageContent);
+};
+
+export default createRestaurantHomePage;
---
+++ src/tabs.js
@@ -0,0 +1,54 @@
+import createContactPage from "./contact";
+import createMenuPage from "./menu";
+import createRestaurantHomePage from "./restaurant";
+
+const createTabs = () => {
+    const content = document.querySelector('#content');
+
+    //Create the three divs
+    const div1 = document.createElement('div');
+    const div2 = document.createElement('div');
+    const div3 = document.createElement('div');
+    
+    //Set ids for the divs
+    div1.setAttribute('id', 'home-btn');
+    div2.setAttribute('id', 'menu-btn');
+    div3.setAttribute('id', 'contact-btn');
+
+    //Set slasses for the divs
+    div1.classList.add('tab');
+    div2.classList.add('tab');
+    div3.classList.add('tab');
+
+    //Set text content for the tabs
+    div1.textContent = 'Home';
+    div2.textContent = 'Menu';
+    div3.textContent = 'Contact';
+
+    //Append the divs to the content div
+    content.appendChild(div1);
+    content.appendChild(div2);
+    content.appendChild(div3);
+
+    div1.addEventListener('click', () => {
+        clearContent();
+        createRestaurantHomePage();
+    })
+    div2.addEventListener('click', () => {
+        clearContent();
+        createMenuPage();
+    })
+    div3.addEventListener('click', () => {
+        clearContent();
+        createContactPage();
+    })
+};
+
+function clearContent() {
+    const content = document.querySelector("#content");
+    const pageContent = document.querySelector("#.page-content");
+    if (pageContent) {
+        content.removeChild(pageContent);
+    }
+};
+export default createTabs;
